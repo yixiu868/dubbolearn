f1649a6765c91a1d10efa9008100db6056a3af45
yixiu868 <yixiu868@163.com>
2021-01-05 23:40:44 +0800

    xml、配置、API方式搭建dubbo

---
+++ dubbo-facade/src/main/java/com/ww/service/UserService.java
@@ -0,0 +1,8 @@
+package com.ww.service;
+
+import com.ww.model.User;
+
+public interface UserService {
+
+    User findUserById(Integer id);
+}
--- spring-dubbo/src/main/java/com/ww/spring/dubbo/demo/provider/service/DemoService.java
+++
@@ -1,6 +0,0 @@
-package com.ww.spring.dubbo.demo.provider.service;
-
-public interface DemoService {
-
-    String sayHello(String name);
-}
