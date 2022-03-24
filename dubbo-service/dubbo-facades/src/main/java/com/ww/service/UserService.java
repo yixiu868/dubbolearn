b38bd22338bd3c0f74d1c7f5730f31152b21d791
yixiu868 <yixiu868@163.com>
2022-03-24 17:58:11 +0800

    删除

--- dubbo-service/dubbo-facades/src/main/java/com/ww/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.ww.service;
-
-import com.ww.model.User;
-
-public interface UserService {
-
-    User findUserById(Integer id);
-}
--- dubbo-service/dubbo-rpc-base/src/main/java/com/ww/spi/java/service/Speaker.java
+++
@@ -1,6 +0,0 @@
-package com.ww.spi.java.service;
-
-public interface Speaker {
-
-    void speak();
-}
